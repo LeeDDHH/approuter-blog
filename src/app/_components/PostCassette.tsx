34e881c42a91aae597d63f2823ac08d4cae462ab
DongHeon Lee <niche3600@gmail.com>
2025-07-06 21:58:10 +0900

    ホバー時のスタイル修正

--- src/app/_components/PostCassette.tsx
+++ src/app/_components/PostCassette.tsx
@@ -16,14 +16,16 @@ const PostCassette = ({
 	return (
 		<>
 			<div className="flex sm:flex-row flex-col justify-between">
-				<h3 className="font-bold text-xl">{title}</h3>
+				<h3 className="font-bold text-xl hover:underline hover:underline-offset-2 hover:text-teal-100">
+					<Link href={`/blog/${slug}`}>{title}</Link>
+				</h3>
 				<time className=" sm:mt-0 mt-2" dateTime={formattedDate}>
 					{formattedDate}
 				</time>
 			</div>
 			<p className="sm:my-6 my-2">{summary}</p>
 			<div className="flex sm:flex-row flex-col gap-2 sm:items-center justify-between mt-4">
-				<div className="flex gap-2 items-center shrink-0">
+				<div className="flex gap-2 items-center shrink-0 hover:underline hover:underline-offset-2 hover:text-teal-300">
 					<Link href={`/blog/${slug}`}>Read more</Link>
 				</div>
 				<AllTags allTags={tags} />
