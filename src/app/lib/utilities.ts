d476ad9c5f09ddad55162d5c77d7aae27cb6fb65
DongHeon Lee <niche3600@gmail.com>
2025-07-06 19:08:16 +0900

    マークダウン解析時にgithub風に読み込むようにする

--- src/app/lib/utilities.ts
+++ src/app/lib/utilities.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
+import remarkGfm from 'remark-gfm';
 
 export type Tags = string[];
 
@@ -68,7 +69,7 @@ async function getPostData(slug:string): Promise<PostData> {
     const fileContents = fs.readFileSync(fullPath, 'utf8');
     const matterResult = matter(fileContents);
 
-    const processedContent = await remark().use(html).process(matterResult.content);
+    const processedContent = await remark().use(remarkGfm).use(html).process(matterResult.content);
     const contentHtml = processedContent.toString();
 
     return {
